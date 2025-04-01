0550abe95b4e144a408416c62e29467fb7d1c92f
Gulzar Fatima <gul.aka.alukebab@gmail.com>
2025-04-01 15:03:05 -0400

    summary
    changes

---
+++ Lab 9/lab9.js
@@ -0,0 +1,34 @@
+﻿//LAB 9-DATA STORAGE: INDEX PAGE
+//window.onload
+
+    //check for stored values
+        //retrieve stored values
+
+        //change welcome text to stored name
+
+        //change BG colour to stored colour
+
+        //  document.body.style.background = 
+
+
+		
+//#####============== DO THIS PART FIRST! ===============		
+    //get the form and set submit listener
+	
+	
+
+	//onsubmit Function
+        
+        //get values from form
+		
+		
+		//console.log the form values
+
+        
+        //store values
+        
+        
+
+
+
+    
